0a28f646582828b4a9cb89051fab545f8426df65
Francesco pippi <Francescopippi02@gmail.com>
2021-01-28 00:33:25 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Pippi_SocketAsyncLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Pippi_AsyncTimeServer
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TimeSocketServer mServer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mServer = new TimeSocketServer();
+        }
+        
+
+        private void Btn_Ascolto_Click(object sender, RoutedEventArgs e)
+        {
+            mServer.InAscolto();
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            mServer.inviaATutti();
+        }
+    }
+}
